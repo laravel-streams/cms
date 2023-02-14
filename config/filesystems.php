be17ee3ddf67fa4827d73b9212876f8d884cb8a7
Ryan Thompson <ryan@pyrocms.com>
2023-02-14 17:01:21 -0600

    Testing

--- config/filesystems.php
+++ config/filesystems.php
@@ -33,7 +33,8 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
-            'throw' => false
+            'throw' => false,
+            'stream' => 'files',
         ],
 
         'public' => [
