69e963ba5a7b2e8075ea11c3f57f2e81e2abefae
Ryan Thompson <ryan@pyrocms.com>
2023-03-07 13:18:53 -0600

    Testing config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Livewire\Livewire;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Livewire::component('contact-form', \App\Components\ContactForm::class);
+        //
     }
 }
