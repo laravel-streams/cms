ad4b4104e018476158e7aadccfb609635f50f5d2
Ryan Thompson <ryan@pyrocms.com>
2023-03-09 17:24:10 -0600

    Updated

--- app/Components/ContactForm.php
+++ app/Components/ContactForm.php
@@ -5,6 +5,7 @@ namespace App\Components;
 use App\Mail\ContactRequest;
 use Streams\Ui\Components\Form;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Request;
 
 class ContactForm extends Form
 {
@@ -20,7 +21,7 @@ class ContactForm extends Form
 
     public string $template = <<<'blade'
         <div>
-            <form wire:submit.prevent="submit" {!! $component->htmlAttributes([
+            <form method="POST" action="/streams/ui/{{ $component->id }}/send" {!! $component->htmlAttributes([
                 'class' => ['flex mx-auto w-72 flex-col space-y-2'],
             ]) !!}>
                 <input
@@ -39,7 +40,7 @@ class ContactForm extends Form
                     class="border p-2"
                     required>
 
-                @livewire('textarea', [
+                @ui('textarea', [
                     'name' => 'message',
                     'placeholder' => 'Message',
                     'value' => $component->message,
@@ -54,8 +55,8 @@ class ContactForm extends Form
         </div>
     blade;
 
-    public function submit()
+    public function send()
     {
-        // Mail the contact request.
+        dd(Request::post());
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Streams\Ui\Support\Facades\UI;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        UI::component('contact-form', \App\Components\ContactForm::class);
     }
 }
